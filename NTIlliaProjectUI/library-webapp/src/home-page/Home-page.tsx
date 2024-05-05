653e3d6c122ef7ee07e538359db5cac070d2d23f
illia_pastushok <276487@student.pwr.edu.pl>
2024-05-05 11:43:28 +0200

    Book view update

--- NTIlliaProjectUI/library-webapp/src/home-page/Home-page.tsx
+++ NTIlliaProjectUI/library-webapp/src/home-page/Home-page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Grid, Paper, Typography } from '@mui/material';
+import kobzar from './kobzar.jpg';
+import witcher from './witcher.jpg';
+import witcher1 from './witcher1.jpg';
+import './Home-page.css';
+import {AppBar, Toolbar,Box, Grid, Card, CardMedia, CardContent, Typography } from '@mui/material';
+import { Container } from '@mui/material';
 
 const HomePage = () => {
 	// Dummy data for books
@@ -8,44 +13,61 @@ const HomePage = () => {
 			id: 1,
 			title: 'Book 1',
 			author: 'Author 1',
-			coverImage: 'book1.jpg',
+			coverImage: kobzar,
 		},
 		{
 			id: 2,
 			title: 'Book 2',
 			author: 'Author 2',
-			coverImage: 'book2.jpg',
+			coverImage: witcher,
 		},
 		{
 			id: 3,
 			title: 'Book 3',
 			author: 'Author 3',
-			coverImage: 'book3.jpg',
+			coverImage: witcher1,
 		},
 		// Add more books as needed
 	];
 
-	return (
-		<Grid container spacing={2}>
-			{books.map((book) => (
-				<Grid item xs={4} key={book.id}>
-					<Paper elevation={3} sx={{ p: 2 }}>
-						<img
-							src={book.coverImage}
-							alt={book.title}
-							style={{ width: '100%' }}
-						/>
-						<Typography variant="h6" sx={{ mt: 1 }}>
-							{book.title}
-						</Typography>
-						<Typography variant="subtitle1" sx={{ mt: 0.5 }}>
-							{book.author}
-						</Typography>
-					</Paper>
-				</Grid>
-			))}
-		</Grid>
-	);
+
+return (
+  <>
+    <AppBar position="static" className="AppBar">
+      <Toolbar>
+        <Typography variant="h6" component="div">
+          My Book Store
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Container className="container">
+      <Box mt={2}>
+        <Grid container spacing={2}>
+          {books.map((book) => (
+            <Grid item xs={12} sm={6} md={4} key={book.id}>
+              <Card className="Card">
+                <CardMedia
+                  component="img"
+                  className="book-cover"
+                  image={book.coverImage}
+                  alt={book.title}
+                />
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {book.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {book.author}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  </>
+);
 };
 
 export default HomePage;
